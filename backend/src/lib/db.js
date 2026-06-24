7294f0ed27ac736a2ca10e75fa0c0ec684cfab9c
Pranshu Saxena <thewebwizard12@gmail.com>
2026-06-24 22:44:53 +0530

    feat: add environment variable configuration and update database connection to use MONGODB_URI

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
 
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/myDatabase");
+        await mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/myDatabase");
         console.log("connected to the database");
     } catch (err) {
         console.log("mongodb connection error:", err);
